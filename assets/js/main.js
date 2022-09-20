61e6f482f650a5bc16ae7a2c336f37a752d2c0a3
cerromty2 <107288189+cerromty2@users.noreply.github.com>
2022-09-19 21:33:52 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,29 +2,56 @@ window.addEventListener('DOMContentLoaded', function() {
     const addProduct = document.querySelector("#addProduct")
     const prodName = document.querySelector("#prodName")
     const imgUrlProd = document.querySelector("#imgUrlProd")
-    const prodDescrip = document.querySelector("#addProduct")
+    const prodDescrip = document.querySelector("#prodDescrip")
     const productsList = document.querySelector("#productsList")
     let prods = ""
 
+
+
+    /*addProduct.addEventListener('click', function(event) {
+        event.preventDefault()*/
     addProduct.addEventListener('click', function(event) {
-        event.preventDefault()
+            event.preventDefault()
 
-        const productName = prodName.value
-        const imgUrl = imgUrlProd.value
-        const productDescrip = prodDescrip.value
+        const producto = prodName.value
+        const imagen = imgUrlProd.value
+        const descripcion = prodDescrip.value
+
+        if (producto === "") {
+            prodName.classList.add('is-invalid')
+          }
+      
+          if (imagen === "") {
+            imgUrlProd.classList.add('is-invalid')
+          }
+    
+          if (descripcion === "") {
+            prodDescrip.classList.add('is-invalid')
+          }
+      
+          if (producto !== "" && imagen !== "" && descripcion !== "") {
+            prodName.classList.remove('is-invalid')
+            imgUrlProd.classList.remove('is-invalid')
+            prodDescrip.classList.remove('is-invalid')
         
 
 
-        const productCard = `<div class="prodCard">
-                <figure><img src="${imgUrl}" alt=""></figure>
+            const productCard = `<div class="prodCard">
+                <figure><img src="${imagen}" class="imgsize" alt=""></figure>
                 <div class="cardContetnt">
-                    <h2 class="prodName">${productName}</h2>
-                    <p class="prodDesc">${productDescrip}</p>
+                    <h2 class="prodName">${producto}</h2>
+                    <p class="prodDesc">${descripcion}</p>
                 </div>
-            </div>`
+              </div>`
         
-        prods +=productCard
-        productsList.innerHTML = prods
+            prods += productCard
+            productsList.innerHTML = prods
+
+            imgUrlProd.value = ""
+            prodName.value = ""
+            prodDescrip.value = ""
+          }
+
 
     })
 })
